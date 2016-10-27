79eaa292b61f199cb5e1c0adf306ca5d4ee4e2c9
IT Class 14 <IT Class 14@192.168.0.100>
2016-10-27 19:02:05 +0300

    scan vk.com

--- 123/src/by/galov/lesson2016_10_27/App.java
+++ 123/src/by/galov/lesson2016_10_27/App.java
@@ -3,13 +3,15 @@ package by.galov.lesson2016_10_27;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		InputStream is = new FileInputStream("in.txt");
-		Scanner sc = new Scanner(is);
+		//InputStream is = new FileInputStream("in.txt");
+		URL url = new URL("http://vk.com");
+		Scanner sc = new Scanner(url.openStream());
 		while(sc.hasNext()){
 			System.out.println(sc.nextLine());
 		}
