818085a00339fced47e38aaf0b8e9464631a64d0
IT Class 14 <IT Class 14@192.168.0.100>
2016-10-27 20:27:49 +0300

    date

--- 123/src/by/galov/lesson2016_10_27/App.java
+++ 123/src/by/galov/lesson2016_10_27/App.java
@@ -1,57 +1,30 @@
 package by.galov.lesson2016_10_27;
 
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
 public class App {
 
-	public static void main(String[] args) throws IOException {
-		String temp = "sadfj";
-		temp = "asdf";
+	public static void main(String[] args) throws InterruptedException  {
+		Date date = new Date();
+		System.out.println(date);
+		Thread.sleep(2000);
+		Date date1 = new Date();
+		System.out.println(date1);
 		
-		String temp3 = "j" + temp.substring(1, temp.length() );
-		StringBuffer buffer = new StringBuffer();
+		System.out.println(date.before(date1));
 		
-		buffer.append("afsdf");
-		buffer.append("asasdfdf");
+		Calendar calendar = Calendar.getInstance();
 		
-		buffer.setCharAt(0, 'g');
+		System.out.println(calendar.get(Calendar.MONTH));
+		System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println(calendar.get(Calendar.YEAR));
 		
-		System.out.println(buffer);
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-mm-dd");
 		
-		//StringBuilder
 		
-		//search
 		
-		for(int i = 0;i < buffer.length();i++){
-			char c = buffer.charAt(i);
-			if (c=='f'){
-				System.out.println("fff");
-			}
-		}
-		
-		//regular expression
-		
-		Pattern pattern = Pattern.compile("[0-9]+");
-		
-		Matcher mat = pattern.matcher(" 234 213 1234 23 2 412 23");
-		
-		System.out.println(mat.matches());
-		
-		System.out.println(mat.find());
-		System.out.println(mat.group());
-		
-		while (mat.find()){
-			System.out.println(mat.group());
-			System.out.println(mat.start());
-			System.out.println(mat.end());
-
-			
-		}
-		
-		String s = mat.replaceAll("number");
-		System.out.println(s);
 	}
 
 }
